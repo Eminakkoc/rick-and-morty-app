d901804e22dcb9b6114d187ec0edfd9032d9b5c4
Emin Akkoc <emin.akkoc@monitise.com>
2019-06-30 11:33:44 +0300

    Assets and MaterialUI
    Added MaterialUI, Rick and Morty Font and asset configurations for webpack

--- src/components/main-page/MainPage.jsx
+++ src/components/main-page/MainPage.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 
 import Page from 'components/page/Page';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardActionArea from '@material-ui/core/CardActionArea';
+
+import PlaceholderImage from 'images/placeholder.jpg';
 
 const MainPage = () => (
   <Page>
     <div className="main-page-container">
       Main Page
     </div>
+    <Card className="card">
+      <CardActionArea>
+        <CardMedia
+          className="card-image"
+          image={PlaceholderImage}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          Example Card
+        </CardContent>
+      </CardActionArea>
+    </Card>
   </Page>
 );
 
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -7,97 +7,108 @@ import autoprefixer from 'autoprefixer';
 
 const SCSS_PATTERN = /\.scss$/;
 const JS_JSX_PATTERN = /\.jsx?$/;
+const ASSET_PATTERN = /\.(jpe?g|png|gif|svg|ttf|otf|eot|woff(2)?)(\?v=\d+)?$/;
 
 const config = {
-    entry: {
-        ['rick-and-morty-app']: [
-            'index.jsx',
-            'index.scss',
-        ],
-    },
-    output: {
-        path: `${__dirname}/dist`,
-        filename: 'bundle.js',
-        publicPath:'/'
-    },
-    module: {
-        rules: [
-            {
-                test: JS_JSX_PATTERN,
-                exclude: /node_modules/,
-                enforce: 'pre',
-                loader: 'eslint-loader'
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: SCSS_PATTERN,
-                enforce: 'pre',
-                loader: 'import-glob-loader',
-            },
-            {
-                test: SCSS_PATTERN,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: () => [autoprefixer],
-                        },
-                    },
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.(html)$/,
-                use: {
-                    loader: 'html-loader',
-                    options: {
-                        attrs: [':data-src'],
-                    },
-                },
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ["babel-loader", "eslint-loader"]
-            }
-        ]
-    },
-    resolve: {
-        modules: [
-            'node_modules',
-            path.resolve(__dirname, 'src'),
-        ],
-        extensions: ['.js', '.jsx', '.json'],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'public/index.html',
-            filename: 'index.html',
-            inject: 'body',
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'rick-and-morty-app.css'
-        }),
-        new StyleLintPlugin({
-            configFile: path.resolve(__dirname, '.stylelintrc.json'),
-            files: 'src/**/*.scss',
-            syntax: 'scss'
-        })
+  entry: {
+    ['rick-and-morty-app']: [
+      'index.jsx',
+      'index.scss',
     ],
-    devServer: {
-        port: 3000,
-        historyApiFallback: true
-    }
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: JS_JSX_PATTERN,
+        exclude: /node_modules/,
+        enforce: 'pre',
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: SCSS_PATTERN,
+        enforce: 'pre',
+        loader: 'import-glob-loader',
+      },
+      {
+        test: SCSS_PATTERN,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer],
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            attrs: [':data-src'],
+          },
+        },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: ASSET_PATTERN,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]?[hash]',
+          context: 'assets',
+        },
+      },
+    ],
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, 'assets'),
+      path.resolve(__dirname, 'src'),
+    ],
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'rick-and-morty-app.css',
+      allChunks: true,
+    }),
+    new StyleLintPlugin({
+      configFile: path.resolve(__dirname, '.stylelintrc.json'),
+      files: 'src/**/*.scss',
+      syntax: 'scss',
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+  },
 };
 
 export default config;
