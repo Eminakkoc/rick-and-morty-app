10c1d4aa1c4f02e965d73580ea59c9345260b707
Eminakkoc <emin.akkoc@gmail.com>
2021-10-14 18:49:09 +0300

    remove text

--- src/components/page/Page.jsx
+++ src/components/page/Page.jsx
@@ -24,7 +24,7 @@ const Page = (props) => {
               </BackButton>
             )
           }
-          {children}circle
+          {children}
         </div>
       )}
     </LanguageConsumer>
