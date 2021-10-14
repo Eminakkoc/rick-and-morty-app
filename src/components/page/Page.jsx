a9b632ca677c58ae2c9f6817cb0465e9796b426e
Eminakkoc <emin.akkoc@gmail.com>
2021-10-14 17:03:01 +0300

    circleci test 1
    test 1

--- src/components/page/Page.jsx
+++ src/components/page/Page.jsx
@@ -24,7 +24,7 @@ const Page = (props) => {
               </BackButton>
             )
           }
-          {children}
+          {children}circle
         </div>
       )}
     </LanguageConsumer>
